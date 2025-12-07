cd048aa4fb4158a4e7368cc14935ec840d5429ad
DarkFury <villegassantanaedwin@gmail.com>
2025-12-07 07:53:06 -0600

    Variables N

--- src/transpilar/variables.js
+++ src/transpilar/variables.js
@@ -21,28 +21,23 @@ export function transpilarVariables(code) {
             //tipos de variables
             //obtiene valor después de "="
             let valor = convertir.split("=")[1]?.trim();
-            let tipo = "";
 
             if (valor) {
 
                 //string
                 if (/^".*"$/g.test(valor) || /^'.*'$/.test(valor)) {
-                    tipo = "str";
                 }
 
                 //entero
                 else if (/^-?\d+$/.test(valor)) {
-                    tipo = "int";
                 }
 
                 //decimal
                 else if (/^-?\d+\.\d+$/.test(valor)) {
-                    tipo = "float";
                 }
 
                 //booleano
                 else if (/^(true|false)$/i.test(valor)) {
-                    tipo = "bool";
                     convertir = convertir
                         .replace(/\btrue\b/gi, "True")
                         .replace(/\bfalse\b/gi, "False");
@@ -50,14 +45,8 @@ export function transpilarVariables(code) {
 
                 //null
                 else if (/^null$/i.test(valor)) {
-                    tipo = "null";
                     convertir = convertir.replace(/\bnull\b/gi, "None");
                 }
-            }
-
-            //agrega comentario del tipo si existe
-            if (tipo !== "") {
-                convertir += `  # ${tipo}`;
             }
 
             //guarda las nuevas variables
